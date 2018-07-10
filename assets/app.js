48362dda756fe0b7f92abd3d2b4af45c45422512
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-07-09 20:55:09 -0500

    adding function for putting gifs on page

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,21 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-    
+
 
     $("button").on("click", function () {
         var submit = $("input").val();
         console.log(submit);
-        
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + submit + "&api_key=Onu3hiVRvdlarqe3KvT3PuRCooAJ9gBo&limit=10"
+        console.log(queryURL)
+
+    
+        $.get(queryURL).then(function (response) {
+                console.log(response.data[0].images.fixed_height.url);
+                 $("#gifs").append("<img src=' " + response.data[0].images.fixed_height.url + "'>")
+            })
+
+
     })
 
 
