3b68eee3caa93ee2fbff8e257d1c17a913779d20
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-07-13 20:50:11 -0500

    making them move and stop

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 
     // array that holds all of the string values of the buttons
-    var topics = ["PUBG", "MarioParty", "OverWatch", "MarioKart", "SmashBros", "CallofDuty", "Halo", "GTA:5"];
+    var topics = ["PUBG", "Mario Party", "OverWatch", "Mario Kart", "Smash Bros", "Call of Duty", "Halo", "GTA:5"];
 
     // this loop appends the buttons to the page on load
     for (j = 0; j < topics.length; j++) {
@@ -12,11 +12,14 @@ $(document).ready(function () {
     $(document).on('click', '.doIt', function(event){
         event.preventDefault();
         gameURL = `http://api.giphy.com/v1/gifs/search?q=${$(this).data('search')}&api_key=Onu3hiVRvdlarqe3KvT3PuRCooAJ9gBo&limit=10`
-        console.log(gameURL);
+
 
             $.get(gameURL).then(function (arr){
+                console.log(arr.data)
                 for (u = 0; u < arr.data.length; u++){
-                $("#gifs").prepend(`<p><img src="${arr.data[u].images.fixed_height.url}"></br>Rating: "${arr.data[u].rating}"</p>`)
+                $("#gifs").prepend(`<p><img src="${arr.data[u].images.fixed_height_still.url}"
+                data-still="${arr.data[u].images.fixed_height_still.url}" data-animate="${arr.data[u].images.fixed_height.url}
+                "data-state"still" class="giffyBoi"></br>Rating: "${arr.data[u].rating}"</p>`)
                 }
             })
 
@@ -34,7 +37,9 @@ $(document).ready(function () {
         //sends api link and appends to page
         $.get(queryURL).then(function (response) {
             for (i = 0; i < response.data.length; i++) {
-                $("#gifs").prepend(`<p><img src="${response.data[i].images.fixed_height.url}"></br>Rating: "${response.data[i].rating}"</p>`)
+                $("#gifs").prepend(`<p><img src="${response.data[i].images.fixed_height_still.url}"
+                data-still="${response.data[i].images.fixed_height_still.url}" data-animate="${response.data[i].images.fixed_height.url}"
+                data-still="still" class="giffyBoi"></br>Rating: "${response.data[i].rating}"</p>`)
             };
 
             $("#buttons").append(`<button type="button" data-search="${submit}" class="btn btn-info float-left doIt">${submit}</button>`)
@@ -46,5 +51,18 @@ $(document).ready(function () {
 
     });
 
+    $(document).on("click", ".giffyBoi", function() {
+        
+        var state = $(this).attr("data-state");
+      
+            if (state === "still") {
+            var animateIt = $(this).attr("data-animate")
+            $(this).attr("src", animateIt);
+            $(this).attr("data-state", "animate");
+            } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            }
+    });
 
 });
